2ba0fa03cecf333898effbae53bb0037e2de1340
Mario Cocca <mario.cocca02@sfr.fr>
2021-09-29 09:54:01 +0200

    mise en place ddu logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App {
+  render() {
+    return (
+      <div className="general">
+        <header className="header">
+          <img src={require("./img/logo-clean3000.png")} className="App-logo" alt="logo" />
+        </header>
+
+        <h1>Bienvenue</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
